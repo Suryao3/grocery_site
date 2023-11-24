d7013099cb51fe3ca0ebff2622896b2a380bc0fe
AutmduGithub <128447545+AutmduGithub@users.noreply.github.com>
2023-11-24 14:49:06 +0530

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx', // Adjust this to your actual entry point
+    }
+  },
   plugins: [react()],
+  
 })
